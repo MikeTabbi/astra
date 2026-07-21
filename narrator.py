80b227d79238f0c527ba3a413f3ea7a9d653ed00
zionreams <152207605+zionreams@users.noreply.github.com>
2026-07-21 11:13:57 -0400

    test

--- narrator.py
+++ narrator.py
@@ -1,7 +1,7 @@
 from ollama import Client
 
 # Zion pastes your ngrok URL here:
-client = Client(host='https://a1b2-c3d4-x5y6.ngrok-free.app')
+client = Client()
 
 # Test call to your MacBook's Ollama instance
 response = client.chat(
